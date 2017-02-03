3de676afeea9b45c61f82deefbc3e8236607e2cd
Axel <axeltantay@gmail.com>
2017-02-03 23:00:47 +0800

    asd

---
+++ app/src/main/java/push/EndPoints.java
@@ -0,0 +1,9 @@
+package push;
+
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "https://pushnotification.host.com/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "https://pushnotification.host.com/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "https://pushnotification.host.com/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "https://pushnotification.host.com/GetRegisteredDevices.php";
+}
